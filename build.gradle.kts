4be06171a5c655fe08c222011589fd325e257bc8
LeeHansHinLun <79551623+LeeHansHinLun@users.noreply.github.com>
2024-01-09 22:46:24 -0600

    Include libreforge in build files

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ allprojects {
         compileOnly("com.willfp:eco:6.55.0")
         compileOnly("org.jetbrains:annotations:23.0.0")
         compileOnly("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
+        implementation("com.willfp.libreforge:${libreforgeVersion}")
     }
 
     java {
@@ -51,6 +52,7 @@ allprojects {
         shadowJar {
             relocate("com.willfp.libreforge.loader", "com.willfp.ecojobs.libreforge.loader")
             relocate("com.willfp.ecomponent", "com.willfp.ecojobs.ecomponent")
+            relocate("com.willfp.libreforge", "com.willfp.ecojobs.libreforge")
         }
 
         compileKotlin {
